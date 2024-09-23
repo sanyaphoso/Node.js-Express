e110502f039b08541245462531d213b53356e0ae
sanyaphoso <sanyazza.nat@gmail.com>
2024-09-23 09:02:18 +0700

    update public file

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // เรียกใช้งาน express
 const express = require('express');
-// const debug = require('debug');
+// const debug = require('debug')('app');
 // const morgan = require('morgan');
 const app = express();
 // port
 const port = 3000;
+
+const path = require('path')
+
+// บอกให้รู้ว่า static file อยู่ที่ไหน
+app.use(express.static(path.join(__dirname, "/public/")))
 
 // จัดการ require
 app.get("/", (req, res) =>{
